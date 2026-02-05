2e2ede9a581a9c0151255af0c8f1d9cb7b704f4b
vyaknil <vyaknil@gmail.com>
2026-02-05 21:25:15 +0400

    test db

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'vyaknil-next';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  output: "export",
-  basePath: isProd? `/${repoName}` : "",
-  assetPrefix: isProd? `/${repoName}/` : "",
-  images: {
-    unoptimized: true
-  }
+  reactCompiler: true
 };
 
 export default nextConfig;
